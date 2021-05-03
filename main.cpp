b663363f47f866d3010e776e851d5b163f5bbb51
Ilya Nidzelsky <inidzelsky@gmail.com>
2021-05-03 23:58:58 +0300

    refactor variables names. set intersection algorithm in the main function and send to the render function as an argument

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <cmath>
 
 #include "imageInfo.hpp"
-#include "objreader.hpp"
 
 // Geometry
 #include "vector.hpp"
@@ -24,28 +23,26 @@
 
 using namespace std;
 
-void render(ImageInfo imageInfo, Vector camera_pos, Vector screen_pos, vector<Triangle> triangles, vector<bool> &rendered_pixels) {
-    IntersectionAlgorithm *intersectionAlgorithm;
-    intersectionAlgorithm = new MollerTrumboreAlgorithm();
-    double image_ratio = imageInfo.width / (double)imageInfo.height;
+void render(ImageInfo imageInfo, IntersectionAlgorithm *intersectionAlgorithm, Vector cameraPos, Vector screenPos, vector<Triangle> triangles, vector<bool> &renderedRixels) {
+    double imageRatio = imageInfo.width / (double)imageInfo.height;
     double fov = 60;
-    double scale = tan(fov / 180 * M_PI) * (screen_pos - camera_pos).len();
+    double scale = tan(fov / 180 * M_PI) * (screenPos - cameraPos).len();
 
     for (int i = 0; i < imageInfo.height; i++) {
         for (int j = 0; j < imageInfo.width; j++) {
-            double pos_x = (2 * (j + 0.5) / (double)imageInfo.width - 1) * image_ratio * scale;
-            double pos_y = (1 - 2 * (i + 0.5) / (double)imageInfo.height) * scale;
+            double posX = (2 * (j + 0.5) / (double)imageInfo.width - 1) * imageRatio * scale;
+            double posY = (1 - 2 * (i + 0.5) / (double)imageInfo.height) * scale;
 
-            Vector direction = Vector(pos_x, pos_y, screen_pos.z).normalize();
-            bool is_intersection = false;
+            Vector direction = Vector(posX, posY, screenPos.z).normalize();
+            bool isIntersection = false;
 
             for (int o = 0; o < triangles.size(); o++) {
                 Triangle triangle = triangles[o];
-                is_intersection = intersectionAlgorithm->checkIntersection(camera_pos, direction, triangle);
-                if (is_intersection) break;
+                isIntersection = intersectionAlgorithm->checkIntersection(cameraPos, direction, triangle);
+                if (isIntersection) break;
             }
 
-            rendered_pixels.push_back(is_intersection ? 1 : 0);
+            renderedRixels.push_back(isIntersection ? 1 : 0);
         }
     }
 }
@@ -56,23 +53,26 @@ int main() {
     sceneReader = new ObjReader();
     vector<Triangle> triangles = sceneReader->readScene("objects/cube.obj");
     
-    // Set final image info
+    // Set the image info
     ImageInfo imageInfo(1080, 720);
 
     // Define camera and screen position
-    Vector camera_pos(0, 0, 10);
+    Vector cameraPos(0, 0, 10);
     Vector direction (0, 0, -1);
-    Vector screen_pos = camera_pos - direction;
+    Vector screenPos = cameraPos - direction;
+
+    // Set the intersection algorithm
+    IntersectionAlgorithm *intersectionAlgorithm;
+    intersectionAlgorithm = new MollerTrumboreAlgorithm();
 
     // Define pixels buffer
     vector<bool> renderedPixels;
 
     // Render the scene
-    render(imageInfo, camera_pos, screen_pos, triangles, renderedPixels);
+    render(imageInfo, intersectionAlgorithm, cameraPos, screenPos, triangles, renderedPixels);
 
     // Write pixels with the writer implementation
     ImageWriter *imageWriter;
     imageWriter = new PpmImageWriter();
-
     imageWriter->write(imageInfo, renderedPixels, "out.ppm");
 }
